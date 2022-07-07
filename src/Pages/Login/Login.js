b636c5c972769f182317ca91c06b707487a9432b
syadur-killa <syadur.killa@gmail.com>
2022-07-07 10:42:04 +0600

    seccure firebaseinit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useNavigate, Link } from 'react-router-dom';
 
 const Login = () => {
-    return (
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate()
+
+    const handelSubmit= (event) => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        console.log(email, password)
+    }
+
+        return (
         <div className='container w-50 mx-auto'>
             <h2 className='text-primary'>Please Login</h2>
-            <Form>
+            <Form onSubmit={handelSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -16,7 +29,7 @@ const Login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
@@ -25,6 +38,7 @@ const Login = () => {
                     Submit
                 </Button>
             </Form>
+            <p>New to Here!! <Link to="/signup" className='text-danger pe-auto text-decoration-none'>Please Registered</Link></p>
         </div>
     );
 };
